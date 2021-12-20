dd394d689018c73d8574d946b86b7eb11d64083c
paunicolau22 <94358067+paunicolau22@users.noreply.github.com>
2021-12-20 10:31:03 +0100

    Sense PrintTwo

--- Practica_Pau Nicolau/src/Practica_Pau.java
+++ Practica_Pau Nicolau/src/Practica_Pau.java
@@ -3,17 +3,13 @@ public class Practica_Pau {
 		public static void main(String[] args) {
 			printOne();
 			printOne();
-			printTwo();
 		}
 
 		public static void printOne() {
 			System.out.println("Hello World");
 		}
 
-		public static void printTwo() {
-			printOne();
-			printOne();
-		}
+		
 	}
 
 
